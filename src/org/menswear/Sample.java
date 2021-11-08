9add48e513ee81a87140e35a17c24a32644bb302
sengathir96 <sengathir100@gmail.com>
2021-11-08 20:28:11 +0530

    javaproject

---
+++ src/org/menswear/Sample.java
@@ -0,0 +1,18 @@
+package org.menswear;
+
+public class Sample {
+
+	public static void add1(int a, int b) {
+		
+		int c = a+b;
+		
+		System.out.println(c);
+
+	}
+
+	public static void main(String[] args) {
+		
+		add1(10,20);
+	}
+
+}
